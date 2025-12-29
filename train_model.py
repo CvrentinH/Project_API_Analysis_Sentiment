c867679e13242a4dba0c5a6bed81e5e529a0e697
Corentin HELLIER <37554690+CvrentinH@users.noreply.github.com>
2025-12-29 02:17:50 +0100

    Translate comments and print statements to English
    Updated comments and print statements to English for consistency.

--- train_model.py
+++ train_model.py
@@ -7,51 +7,44 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
 
-#Chargement CSV
+# Loading CSV
 
-print("Chargement des données")
-try : 
-    df = pd.read_csv("dataset.csv")
-    df = df.dropna()
-    df['text'] = df['text'].str.strip()
-    df['sentiment'] = df['sentiment'].str.strip()
-    print(f"Data chargé {len(df)} lignes")
-except FileNotFoundError :
-    print("Erreur fichier introuvable")
-    exit()
 
-#Définition features
+df = pd.read_csv("dataset.csv")
+df = df.dropna()
+df['text'] = df['text'].str.strip()
+df['sentiment'] = df['sentiment'].str.strip()
+
+# Features
 
 x = df["text"]
 y = df["sentiment"]
-
-#Séparation
 
 X_train, X_test, Y_train, Y_test = train_test_split(x,y,test_size=0.2, random_state = 42)
 print(f"Training {len(X_train)}")
 print(f"Test{X_test}")
 
-#Création pipeline
+# Pipeline
 
 pipeline = Pipeline([
     ('tfidf', TfidfVectorizer(lowercase=True, stop_words = "english")),
     ('classifier', LogisticRegression())
 ])
 
-#Entrainement
+# Training
 
-print("Entrainement")
+print("Training")
 pipeline.fit(X_train, Y_train)
 
-#Evaluation du modèle
+# Evaluating model
 
-print("Evaluation")
+print("Evaluating")
 Y_pred = pipeline.predict(X_test)
 accuracy = accuracy_score(Y_test, Y_pred)
-print(f"Accuracy : {accuracy:.2f} (Signifiant {accuracy*100:.0f}% prédiciton correctes sur des données non vues)")
+print(f"Accuracy {accuracy:.2f} ({accuracy*100:.0f}% correct predictions)")
 
 #Export
 
 model_filename = "sentiment_model.pkl"
 joblib.dump(pipeline, model_filename)
-print(f"Modele sauvegardé sous le nom {model_filename}")
+print(f"Model saved under the name {model_filename}")
